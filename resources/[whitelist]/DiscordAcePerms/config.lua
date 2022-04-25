c5725104399cf12fa8da7e56083175d6727da627
MMagnusss <magnuspj08@gmail.com>
2022-04-25 15:42:53 +0200

    Opdater tilladelser

--- resources/[whitelist]/DiscordAcePerms/config.lua
+++ resources/[whitelist]/DiscordAcePerms/config.lua
@@ -1,15 +1,13 @@
 Config = {
 	Server_Name = "Server Navn",
-	Discord_Link = 'Indsæt discord link her',
+	Discord_Link = 'Discord_Link',
 	Website_Link = '',   -- KOMMER SNART
 	roleList = {
-		{911697271861698633, "group.staff"}, -- Staff team rank
-		{940645486170365972, "group.admin"}, -- Prøve admin rank
-		{911697271861698639, "group.admin"}, -- ADmin Rank
-		{911697271882661959, "group.admin"}, -- Senior Admin 
-		{911697271882661961, "group.admin"}, -- Manager rank
-		{911697271882661960, "group.admin"}, -- LEDELSE RANK
-		{911697271899435078, "group.ejer"}, -- EJER RANK
-		{911697271882661968, "group.ejer"}, -- MEDEJER RANK
+		{STAFFTEAM_RANK_ID, "group.staff"}, -- Staff team rank
+		{STAFFTEAM_RANK_ID, "qbcore.mod"}, -- Staff team rank
+		{ADMIN_RANK_ID, "group.admin"}, -- Admin rank
+		{ADMIN_RANK_ID, "qbcore.admin"}, -- Admin Rank
+		{EJER_RANK_ID, "group.ejer"}, -- EJER RANK
+		{EJER_RANK_ID, "qbcore.god"}, -- EJER RANK
 	},
 }
