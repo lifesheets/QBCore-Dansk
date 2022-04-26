c0ff6d2ec8ed4a300c3f7a669b292801b187987c
MMagnusss <magnuspj08@gmail.com>
2022-04-26 14:59:26 +0200

    Tilføj fiske script

---
+++ resources/[nye-scripts]/qb-fishing/client.lua
@@ -0,0 +1,165 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
+-- Eventos
+RegisterNetEvent('mt-fishing:client:MenuCompras', function(data)
+    exports['qb-menu']:openMenu({
+        {
+            header = "Fishing Shop",
+            isMenuHeader = true,
+        },
+        {
+            header = "Fishing Rod",
+            txt = "Price: <br> 100$",
+            params = {
+                event = "mt-fishing:client:ComprarCana",
+            }
+        },
+        {
+            header = "Fishing Bait",
+            txt = "Price: <br> 10$",
+            params = {
+                event = "mt-fishing:client:ComprarIsco",
+            }
+        },
+        {
+            header = "Rent a Boat",
+            txt = "Price: <br> 1000$",
+            params = {
+                event = "mt-fishing:client:AlugarBarco",
+            }
+        },
+        {
+            header = "< Close",
+            txt = "",
+            params = {
+                event = "qb-menu:closeMenu",
+            }
+        },
+    })
+end)
+
+RegisterNetEvent('mt-fishing:client:ComprarCana', function(data)
+    TriggerServerEvent('mt-fishing:server:ComprarCana')
+    TriggerEvent('mt-fishing:client:MenuCompras')
+end)
+
+RegisterNetEvent('mt-fishing:client:ComprarIsco', function(data)
+    TriggerServerEvent('mt-fishing:server:ComprarIsco')
+    TriggerEvent('mt-fishing:client:MenuCompras')
+end)
+
+RegisterNetEvent('mt-fishing:client:AlugarBarco', function(data)
+    local vehicle = 'dinghy'
+    local coords = vector4(-1600.0, 5211.65, 0.15, 115.64)
+    TriggerServerEvent('mt-fishing:server:RemoverDinheiroAluger')
+    QBCore.Functions.SpawnVehicle(vehicle, function(veh)
+        SetVehicleNumberPlateText(veh, "FISH"..tostring(math.random(1000, 9999)))
+        exports['LegacyFuel']:SetFuel(veh, 100.0)
+        --SetEntityHeading(veh, coords.h)
+        TaskWarpPedIntoVehicle(GetPlayerPed(-1), veh, -1)
+        TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
+        SetVehicleEngineOn(veh, true, true)
+    end, coords, true) 
+end)
+
+RegisterNetEvent('mt-fishing:client:AbrirLoja', function(data)
+    QBCore.Functions.Progressbar('fishing_store', 'TALKING TO EMPLOYEE...', 1000, false, true, { -- Name | Label | Time | useWhileDead | canCancel
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function() -- Play When Done
+        TriggerServerEvent("InteractSound_SV:PlayOnSource", "monkeyopening", 0.1)
+        TriggerEvent('mt-fishing:client:MenuCompras')
+    end)
+end)
+
+RegisterNetEvent('mt-fishing:client:MenuVendas', function(data)
+    QBCore.Functions.Progressbar('fishing_store', 'TALKING TO EMPLOYEE...', 1000, false, true, { -- Name | Label | Time | useWhileDead | canCancel
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function() -- Play When Done
+        TriggerServerEvent("InteractSound_SV:PlayOnSource", "monkeyopening", 0.1)
+        exports['qb-menu']:openMenu({
+            {
+                header = "Sell Fish Employee",
+                isMenuHeader = true,
+            },
+            {
+                header = "Sell All Fish",
+                txt = "You'll sell all fish that you have on you <br> Prices: <br> Exotic Fish - 100$ <br> Fish - 10$",
+                params = {
+                    event = "mt-fishing:client:VenderPeixe",
+                }
+            },
+            {
+                header = "< Close",
+                txt = "",
+                params = {
+                    event = "qb-menu:closeMenu",
+                }
+            },
+        })
+    end)
+end)
+
+RegisterNetEvent('mt-fishing:client:VenderPeixe', function(data)
+    TriggerServerEvent('mt-fishing:server:VenderPeixe')
+end)
+
+
+RegisterNetEvent('mt-fishing:client:ComecarPesca', function(data)
+    local ped = PlayerPedId()
+    local time = math.random(1000, 5000)
+    local pos = GetEntityCoords(ped)
+
+    QBCore.Functions.TriggerCallback('QBCore:HasItem', function(HasItem)
+        if HasItem and GetWaterHeight(pos.x, pos.y, pos.z-2, pos.z - 3.0) then
+            local ped = PlayerPedId()
+            local animDict = "amb@world_human_stand_fishing@idle_a"
+            local animName = "idle_c" 
+            local pedPos = GetEntityCoords(ped)
+            local fishingRodHash = `prop_fishing_rod_01`
+            local bone = GetPedBoneIndex(ped, 18905)
+            RequestAnimDict(animDict)
+			while not HasAnimDictLoaded(animDict) do
+				Wait(100)
+			end
+			TaskPlayAnim(ped, animDict, animName, 1.0, -1.0, 1.0, 11, 0, 0, 0, 0)   
+            rodHandle = CreateObject(fishingRodHash, pedPos, true)
+            AttachEntityToEntity(rodHandle, ped, bone, 0.1, 0.05, 0, 80.0, 120.0, 160.0, true, true, false, true, 1, true)
+            Wait(time)
+            local success = exports['qb-lock']:StartLockPickCircle(5,20)
+            if success then
+                TriggerServerEvent('mt-fishing:server:DarPeixe')
+                ClearPedTasks(ped)
+                DeleteObject(fishingRodHash)
+                DeleteEntity(fishingRodHash)
+            else
+                TriggerServerEvent('mt-fishing:server:PerderIsco')
+                QBCore.Functions.Notify('The fish scape...', 'error', 7500)
+                ClearPedTasks(ped)
+                DeleteObject(fishingRodHash)
+                DeleteEntity(fishingRodHash)
+            end
+        else
+        QBCore.Functions.Notify("You dont have any fishing bait or not near water...", "error")
+        end
+      end, 'isco_pesca')
+end)
+
+-- Threads
+Citizen.CreateThread(function()
+    local blip = AddBlipForCoord(-1592.64, 5202.2, 4.31)
+	SetBlipSprite(blip, 88)
+	SetBlipDisplay(blip, 4)
+	SetBlipScale(blip, 0.7)
+	SetBlipAsShortRange(blip, true)
+	SetBlipColour(blip, 37) -- Mudar cor aqui!
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentSubstringPlayerName("Fishing Shop")
+    EndTextCommandSetBlipName(blip)
+end)
---
+++ resources/[nye-scripts]/qb-fishing/server.lua
@@ -0,0 +1,105 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- Eventos
+QBCore.Functions.CreateUseableItem("cana_pesca", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+	if Player.Functions.GetItemBySlot(item.slot) ~= nil then
+        TriggerClientEvent('mt-fishing:client:ComecarPesca', source)
+    end
+end)
+
+RegisterNetEvent('mt-fishing:server:PerderIsco', function(data)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local quantity = 1
+
+    Player.Functions.RemoveItem('isco_pesca', quantity)
+end)
+
+RegisterNetEvent('mt-fishing:server:DarPeixe', function(data)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local quantity = math.random(1, 5)
+    local prob = math.random(1, 100)
+
+    if prob < 50 and prob < 30 then
+        if Player.Functions.AddItem("peixe_pesca", quantity) then
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["peixe_pesca"], 'add')
+        else
+            QBCore.Functions.Notify('You have full pockets.', 'error', 7500)
+        end  
+    elseif prob >= 40 and prob < 80 then
+        if Player.Functions.AddItem("peixe_exotico_pesca", quantity) then
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["peixe_exotico_pesca"], 'add')
+        else
+            QBCore.Functions.Notify('You have full pockets.', 'error', 7500)
+        end
+    else
+        QBCore.Functions.Notify('You dont hit any fish', 'error', 7500)
+    end
+end)
+
+RegisterNetEvent('mt-fishing:server:ComprarCana', function(data)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(source)
+    local quantity = 1
+    local cash = 'bank'
+    local price = 100
+    local Item = 'cana_pesca'
+
+    if Player.Functions.AddItem(Item, quantity) then
+        Player.Functions.RemoveMoney(cash, price)
+        QBCore.Functions.Notify('You buy a Fishing Rod', 'primary', 7500)
+    end
+end)
+
+RegisterNetEvent('mt-fishing:server:ComprarIsco', function(data)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(source)
+    local quantity = 1
+    local cash = 'bank'
+    local price = 10
+    local Item = 'isco_pesca'
+
+    if Player.Functions.AddItem(Item, quantity) then
+        Player.Functions.RemoveMoney(cash, price)
+        QBCore.Functions.Notify('You buy a Fishing Bait', 'primary', 7500)
+    end
+end)
+
+local ItemList = {
+    ["peixe_pesca"] =  10,
+    ["peixe_exotico_pesca"] =  100,
+}
+
+RegisterNetEvent('mt-fishing:server:VenderPeixe', function()
+    local src = source
+    local price = 0
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+    local xItem = Player.Functions.GetItemsByName(ItemList)
+    if xItem ~= nil then
+        for k, v in pairs(Player.PlayerData.items) do
+            if Player.PlayerData.items[k] ~= nil then
+                if ItemList[Player.PlayerData.items[k].name] ~= nil then
+                    price = price + (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                    Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
+
+        Player.Functions.AddMoney("cash", price, "sold-fish")
+            TriggerClientEvent('QBCore:Notify', src, "You sell some fish for $"..price)
+                end
+            end
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, "You dont/'t have anything to sell..")
+    end
+end)
+
+RegisterNetEvent('mt-fishing:server:RemoverDinheiroAluger', function()
+	local src = source
+	local cash = 'bank'
+	local price = 1000
+	local Player = QBCore.Functions.getPlayer(src)
+		
+	Player.Functions.RemoveMoney(cash, price)
+end)
