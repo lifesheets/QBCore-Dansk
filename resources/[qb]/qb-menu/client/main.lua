38817017cf17fa78fca8a888637c13d58f020c21
MMagnusss <magnuspj08@gmail.com>
2022-05-09 17:07:31 +0200

    Fiks igen

--- resources/[qb]/qb-menu/client/main.lua
+++ resources/[qb]/qb-menu/client/main.lua
@@ -1,14 +1,13 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local img = "qb-inventory/html/"
 local headerShown = false
 local sendData = nil
 
 -- Functions
-
 local function openMenu(data)
     if not data or not next(data) then return end
-    for _,v in pairs(data) do 
+	for _,v in pairs(data) do 
 		if v["icon"] then
+			local img = "qb-inventory/html/"
 			if QBCore.Shared.Items[tostring(v["icon"])] then
 				if not string.find(QBCore.Shared.Items[tostring(v["icon"])].image, "images/") then 
 					img = img.."images/"
